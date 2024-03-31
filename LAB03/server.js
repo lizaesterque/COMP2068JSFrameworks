57efed5a039a1f3d8643bfa0dff636196d8889d4
lizaesterque <lizaesterque@live.com>
2024-03-31 16:20:27 -0400

    Update server.js

--- LAB03/server.js
+++ LAB03/server.js
@@ -1,46 +1,52 @@
+//routing with the conncet 
 const connect = require('connect');
 const url = require('url');
 
+
+//creating a request and a res
 const calculate = (req, res) => {
   const parsedUrl = url.parse(req.url, true);
 
+//parses the URL for method, x and y
   const method = parsedUrl.query.method;
   const x = parseFloat(parsedUrl.query.x);
   const y = parseFloat(parsedUrl.query.y);
 
-  let result, operator;
+  let result;
 
+
+//using switch to do the math operation 
   switch (method) {
     case 'add':
       result = x + y;
-      operator = '+';
       break;
     case 'subtract':
       result = x - y;
-      operator = '-';
       break;
     case 'multiply':
       result = x * y;
-      operator = '*';
       break;
     case 'divide':
       result = x / y;
-      operator = '/';
       break;
     default:
       result = 'Invalid method';
   }
 
-  const output = `${x} ${operator} ${y} = ${result}`;
+  //save the output in a cost
+  const output = `${x} ${method} ${y} = ${result}`;
 
+//show the output
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end(` ${output}\n`);
 };
 
+//connect and calculete
 const app = connect();
 
 app.use(calculate);
 
+//show in terminal that the server is running 
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
